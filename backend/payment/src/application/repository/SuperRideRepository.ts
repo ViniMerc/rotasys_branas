0088ce65564c00f5add5869d6ed4caab5626d139
ViniMerc <viniciusmrchede@gmail.com>
2025-11-13 16:17:48 -0300

    wip: payment microservice

--- backend/payment/src/application/repository/SuperRideRepository.ts
+++
@@ -1,7 +0,0 @@
-import Ride from "../../domain/entity/Ride";
-import SuperRide from "../../domain/entity/SuperRide";
-
-export default interface RideRepository {
-	saveRide (ride: Ride): Promise<void>;
-	getRideById (rideId: string): Promise<SuperRide>;
-}
--- backend/payment/src/infra/gateway/MailerGatewayFake.ts
+++
@@ -1,8 +0,0 @@
-import MailerGateway from "../../application/gateway/MailerGateway";
-
-export default class MailerGatewayFake implements MailerGateway {
-
-	async send (recipient: string, subject: string, message: string) {
-		console.log(recipient, subject, message);
-	}
-}
