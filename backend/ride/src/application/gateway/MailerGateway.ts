b7fbde81bc63b939519333f2268c7d8d0cdeff1d
ViniMerc <viniciusmrchede@gmail.com>
2025-11-13 16:22:00 -0300

    wip: add microservice to rides

--- backend/ride/src/application/gateway/MailerGateway.ts
+++
@@ -1,3 +0,0 @@
-export default interface MailerGateway {
-	send (recipient: string, subject: string, message: string): Promise<void>;
-}
--- backend/ride/src/application/gateway/PaymentGateway.ts
+++
@@ -1,3 +0,0 @@
-export default interface PaymentGateway {
-	send (amount: number, trasactionId: string, date:Date): Promise<void>;
-}
