810705c2ee1a3848ce8b3103265033ea73755792
ViniMerc <viniciusmrchede@gmail.com>
2025-11-14 17:45:59 -0300

    fix: http to next

---
+++ frontend_next/app/infra/HttpClient.tsx
@@ -0,0 +1,27 @@
+import HttpClient from "./AccountGateway";
+
+
+// inversão de dependência (DIP)
+export default interface AccountGateway {
+    signup(input: any): Promise<any>;
+}
+
+// interface adapters
+export class AccountGatewayHttp implements AccountGateway {
+
+    constructor(readonly httpClient: HttpClient) {
+    }
+
+    signup(input: any): Promise<any> {
+        return this.httpClient.post("http://localhost:3001/signup", input);
+    }
+
+}
+
+export class AccountGatewayFake implements AccountGateway {
+
+    async signup(input: any): Promise<any> {
+        return { accountId: "123456" }
+    }
+
+}
